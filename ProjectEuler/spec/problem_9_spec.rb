e399fe7dc99020651e03558637cca76f3bc81f20
Nick Kinney <nkinney@virtualhold.com>
2012-09-18 13:30:21 -0400

    completed problem 12

--- ProjectEuler/spec/problem_9_spec.rb
+++ ProjectEuler/spec/problem_9_spec.rb
@@ -4,22 +4,25 @@ require 'spec_helper'
 describe Triangle do
   it 'should give me a and b of the pythagrean theorum for a value of 25 for c' do
     triangle_sides = Triangle.new
-    triangle_sides.set_all_sides_total 12
-    triangle_sides.solve_for_problem_9
+    triangle_sides.find_pythagorean_triplet_with_sides_totaling 12
     triangle_sides.all_sides.should == [3,4,5]
   end
 
   it 'should give me a and b of the pythagrean theorm with a value of 1000 for c' do
     triangle_sides = Triangle.new
-    triangle_sides.set_all_sides_total 1000
-    triangle_sides.solve_for_problem_9
+    triangle_sides.find_pythagorean_triplet_with_sides_totaling 1000
     triangle_sides.all_sides.should == [200,375,425]
   end
 
-  it 'should give me the product of a, b and c when c is know as 100 and a and b are whole numbers' do
+  it 'should give me the product of a, b and c when c is know as 12 and a and b are whole numbers' do
     triangle_sides = Triangle.new
-    triangle_sides.set_all_sides_total 1000
-    triangle_sides.solve_for_problem_9
+    triangle_sides.find_pythagorean_triplet_with_sides_totaling 12
+    triangle_sides.product_all_sides.should == 60
+  end
+
+  it 'should give me the product of a, b and c when c is know as 1000 and a and b are whole numbers' do
+    triangle_sides = Triangle.new
+    triangle_sides.find_pythagorean_triplet_with_sides_totaling 1000
     triangle_sides.product_all_sides.should == 31875000
   end
 end
--- Projecteuler/project_euler/triangle.rb
+++ Projecteuler/project_euler/triangle.rb
@@ -8,19 +8,20 @@ class Triangle
 
 	def set_all_sides_total total
 		@sides_total = total
-	end
+  end
 
-	def solve_for_problem_9
-		(1..@sides_total).each do |a|
-			(a..@sides_total).each do |b|
-				if (a*a)+(b*b) == (@sides_total-a-b)*(@sides_total-a-b)
-				    @a = a
-				    @b = b
-				    @c = @sides_total-a-b
-				end
-			end
-		end		
-	end
+  def find_pythagorean_triplet_with_sides_totaling total
+    set_all_sides_total total
+    (1..@sides_total).each do |a|
+      (a..@sides_total).each do |b|
+        if (a*a)+(b*b) == (@sides_total-a-b)*(@sides_total-a-b)
+            @a = a
+            @b = b
+            @c = @sides_total-a-b
+        end
+      end
+    end
+  end
 
 	def all_sides
 		[@a,@b,@c]
@@ -33,5 +34,27 @@ class Triangle
 	def is_sqrt_whole? num
 		sqrt_num = Math.sqrt num
 		sqrt_num == sqrt_num.to_i
-	end
+  end
+
+  def self.first_triangle_number_to_have_minimum_factors_of number
+    triangle_number = 0
+    divisors = 0
+    side = 0
+    while divisors < number
+      side += 1
+      triangle_number = (side*(side+1))/2
+      divisors = Triangle.number_of_divisors_for(triangle_number)
+    end
+    triangle_number
+  end
+
+  def self.number_of_divisors_for number
+    divisor_number = 0
+    for i in (1..(Math.sqrt(number).to_i)) do
+      if (number % i) == 0
+        divisor_number += 2
+      end
+    end
+    divisor_number
+  end
 end
